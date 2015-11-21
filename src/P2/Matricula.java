9666dece109c7356b761f71e2450b41024f21b4e
albguti <albertogdc91@gmail.com>
2015-11-21 13:59:28 +0100

    Empezamos P2
    Empezamos la P2

---
+++ src/P2/Matricula.java
@@ -0,0 +1,43 @@
+package P2;
+/**
+ * Representa la inscripcion de un alumno en un curso concreto.
+ * 
+ * @author oscafer
+ * @author albguti
+ * @author serorca
+ * 
+ */
+public class Matricula {
+	private String idMatricula;
+	private boolean estado;
+	
+	// Constructor
+	public Matricula(String idMatricula){
+		this.idMatricula= idMatricula;
+		this.estado=false;
+	}
+
+	/**
+	 * Devuelve el identificador de la matricula.
+	 * @return idMatricula
+	 */
+	public String getIdMatricula() {
+		return idMatricula;
+	}
+	
+	/**
+	 * Devuelve el estado en que se encuentra la matricula
+	 * @return true/false (pagada/pendiente)
+	 */
+	public boolean getEstado() {
+		return estado;
+	}
+	
+	/**
+	 * Cambia el estado de la matricula.
+	 * @param estado true/false
+	 */
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+}
